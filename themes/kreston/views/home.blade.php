a37246bda5f1653ec5a7bd0ec3f6890b72cbed29
suhe <hendarsyahss@gmail.com>
2016-09-29 00:07:20 +0700

    upadte home news

--- themes/kreston/views/home.blade.php
+++ themes/kreston/views/home.blade.php
@@ -211,13 +211,68 @@
 		</div>
 	</div>
 	
-	<div class="container" style="margin-top:120px">
-		<div class="row">
-		</div>
-	</div>
 	
   </article>
 </section><!-- #main -->
+
+<div class="slider progressive-slider load">
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="box_style_1">
+                    <div><span style="font-size:24px;font-weight:500">Article</span><span class="pull-right"><a href="http://iapi.or.id/category/69-1-Berita">Lihat Semua <i class="icon-right-dir"></i></a></span></div>
+                    <hr>
+                    <ul class="list_1">
+                    
+                          <li><a href="http://iapi.or.id/detail/169-Bea-Cukai-dan-IAPI-Edukasi-Akuntan-Publik"><i class="fa fa-caret-right"></i> Bea Cukai dan IAPI Edukasi Akuntan Publik</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/168-UNIKAMA-Siap-Uji-Sertifikasi-Akuntan-Publik"><i class="fa fa-caret-right"></i> UNIKAMA Siap Uji Sertifikasi Akuntan Publik</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/166-Materi-PPL-IKNB-8-September-2016"><i class="fa fa-caret-right"></i> Materi PPL IKNB 8 September 2016</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/164-PPL-Khusus-Staf-1-Agustus-2016-UPDATE-EXPOSURE-DRAFT-SPAP"><i class="fa fa-caret-right"></i> PPL Khusus Staf 1 Agustus 2016 - UPDATE EXPOSURE DRAFT…</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/162-Audit-Debitur-LPDB-KUMKM"><i class="fa fa-caret-right"></i> Audit Debitur LPDB KUMKM</a></li>
+                   
+                         <li><a href="http://iapi.or.id/detail/154-IAPI-Mengadakan-Buka-Puasa-Bersama-Ketua-BPK-RI"><i class="fa fa-caret-right"></i> IAPI Mengadakan Buka Puasa Bersama Ketua BPK RI</a></li>
+                                     
+                       <li><a href="http://iapi.or.id/detail/157-UU-Nomor-11-Tahun-2016-Tentang-Pengampunan-Pajak"><i class="fa fa-caret-right"></i> UU Nomor 11 Tahun 2016 Tentang Pengampunan Pajak</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/156-Paparan-Mengenai-Tax-Amnesty"><i class="fa fa-caret-right"></i> Paparan Mengenai Tax Amnesty</a></li>
+                   
+                         
+           </div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="box_style_1">
+                    <div><span style="font-size:24px;font-weight:500"> News</span><span class="pull-right"><a href="http://iapi.or.id/category/69-1-Berita">Lihat Semua <i class="icon-right-dir"></i></a></span></div>
+                    <hr>
+                    <ul class="list_1">
+                    
+                          <li><a href="http://iapi.or.id/detail/169-Bea-Cukai-dan-IAPI-Edukasi-Akuntan-Publik"><i class="fa fa-caret-right"></i> Bea Cukai dan IAPI Edukasi Akuntan Publik</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/168-UNIKAMA-Siap-Uji-Sertifikasi-Akuntan-Publik"><i class="fa fa-caret-right"></i> UNIKAMA Siap Uji Sertifikasi Akuntan Publik</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/166-Materi-PPL-IKNB-8-September-2016"> <i class="fa fa-caret-right"></i> Materi PPL IKNB 8 September 2016</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/164-PPL-Khusus-Staf-1-Agustus-2016-UPDATE-EXPOSURE-DRAFT-SPAP"><i class="fa fa-caret-right"></i> PPL Khusus Staf 1 Agustus 2016 - UPDATE EXPOSURE DRAFT…</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/162-Audit-Debitur-LPDB-KUMKM"><i class="fa fa-caret-right"></i> Audit Debitur LPDB KUMKM</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/157-UU-Nomor-11-Tahun-2016-Tentang-Pengampunan-Pajak"><i class="fa fa-caret-right"></i> UU Nomor 11 Tahun 2016 Tentang Pengampunan Pajak</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/156-Paparan-Mengenai-Tax-Amnesty"><i class="fa fa-caret-right"></i> Paparan Mengenai Tax Amnesty</a></li>
+                   
+                          <li><a href="http://iapi.or.id/detail/154-IAPI-Mengadakan-Buka-Puasa-Bersama-Ketua-BPK-RI"><i class="fa fa-caret-right"></i> IAPI Mengadakan Buka Puasa Bersama Ketua BPK RI</a></li>
+                    </ul>
+                     
+           </div>
+			</div>
+		</div>
+	</div>
+</div>
 
 </div><!-- .page-box-content -->
 </div><!-- .page-box -->
