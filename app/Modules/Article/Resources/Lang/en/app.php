6f5128b7385d2f2f40e9edb0edd7e5ddb0d6b1f8
suhe <hendarsyahss@gmail.com>
2016-10-11 21:03:14 +0700

    Article/Http/Controllers/Administrator/ArticleController.php

---
+++ app/Modules/Article/Resources/Lang/en/app.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BDO-IT
+ * Date: 29/09/2016
+ * Time: 9:06
+ */
+return array (
+    'article' => 'Article',
+	'description' => 'Description',
+    'content' => 'Content',
+    'title' => 'Title',
+    'total view' => 'Total View',
+);
---
+++ app/Modules/Article/Resources/Views/read.blade.php
@@ -0,0 +1,42 @@
+@extends('kreston::post')
+@section('breadcrumbs', Breadcrumbs::render('article_read'))
+@section('content')
+    <div class="row">
+      <div class="content blog blog-post col-sm-9 col-md-9">
+		<article class="post">
+		  <h4 class="title">{!! $post->title !!}</h4>	
+		  <div class="entry-content">
+		  {!! $post->content !!}
+		  </div>
+		  <!--<footer class="entry-meta">
+			<span class="autor-name">Mike Example</span>,
+			<span class="time">03.11.2012</span>
+			<span class="separator">|</span>
+			<span class="meta">Posted in <a href="#">Sports</a>, <a href="#">Movies</a></span>
+			<span class="comments-link pull-right">
+			  <a href="#"><i class="fa fa-comment"></i> 3 comment(s)</a>
+			</span>
+		  </footer>-->
+		</article><!-- .post -->	
+      </div><!-- .content -->
+	  
+      <div id="sidebar" class="sidebar col-sm-3 col-md-3">
+		<aside class="widget list">
+		  
+		
+		<aside class="widget list">
+		  <header>
+			<h3 class="title">Archives</h3>
+		  </header>
+		  <ul>
+			@foreach($post_archieves as $key => $row)
+				<li class="active"><a href="{!! url('article/archieve/'.$row->url) !!}">{!! $row->periode !!}</a></li>
+			@endforeach
+		  </ul>
+		</aside><!-- .list -->
+	
+	  </div><!-- .sidebar -->
+    </div
+@endsection
+
+
